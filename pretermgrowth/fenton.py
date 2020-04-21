3165a3aa6fe209aa32ed92da8e60aef9dd266d4a
gammaflauge <mddevine@gmail.com>
2020-04-20 23:21:49 -0400

    :recycle: Put fenton code into its own class

--- pretermgrowth/fenton.py
+++ pretermgrowth/fenton.py
@@ -1,80 +1,102 @@
 import csv
 from pathlib import Path
+from typing import Dict, List
 
 
-lms_rows = []
-lms_file_path = Path(__file__).resolve().parents[0] / "fentonlms.csv"
-# for interactive development:
-# lms_file = Path(".") / "fentongc" / "fentonlms.csv"
-with open(lms_file_path, "r") as lms_file:
-    for row in csv.DictReader(lms_file):
-        row["gestational_age_in_days"] = int(row["gestational_age_in_days"])
-        row["l"] = float(row["l"])
-        row["m"] = float(row["m"])
-        row["s"] = float(row["s"])
-        lms_rows.append(row)
+class Fenton:
+    lms_rows = None
 
+    @classmethod
+    def _get_lms_rows(cls) -> List[Dict]:
+        """Gets the LMS rows from the fentonlms.csv file and returns them.
 
-def get_lms(
-    metric: str, sex: str, gestational_age_in_days: int
-) -> (float, float, float):
-    """returns the LMS parameters from Fenton tables.
+        This should only be called from `get_lms` and only once per instantiation of
+        class Fenton. I'm not sure if this is the best way to initialize this class
+        variable (`lms_rows`).
 
-    Fenton TR and Kim JH, BMC Pediatrics 2013, 13:59
-    http://www.ncbi.nlm.nih.gov/pubmed/23601190
+        Returns:
+            List[Dict] -- LMS rows in a dict, one row per gestational_age_in_days
+        """
+        rows = []
+        lms_file_path = Path(__file__).resolve().parents[0] / "fentonlms.csv"
 
-    Arguments:
-        metric {str} -- Metric to calculate z-score for. Must be `weight`, `length`,
-            or `hc` (head circumference). Units must be in grams for weight, or cm
-            for length or hc.
-        sex {str} -- Patient sex. Must be `f` or `m`.
-        gestational_age_in_days {int} -- Patient gestational age. Ie, to calculate
-            z-scores for a baby born at 24 weeks 0d on dol 5:
-            gestational_age_in_days = 24 * 7 + 5 = 173
+        with open(lms_file_path, "r") as lms_file:
+            for row in csv.DictReader(lms_file):
+                row["gestational_age_in_days"] = int(row["gestational_age_in_days"])
+                row["l"] = float(row["l"])
+                row["m"] = float(row["m"])
+                row["s"] = float(row["s"])
+                rows.append(row)
+        print("getting lms rows")
 
-    Returns:
-        float -- l parameter
-        float -- m parameter (mean)
-        float -- s parameter
-    """
-    for row in lms_rows:
-        if (
-            row["metric"] == metric
-            and row["sex"] == sex
-            and row["gestational_age_in_days"] == gestational_age_in_days
-        ):
-            return row["l"], row["m"], row["s"]
+        return rows
 
-    raise ValueError(
-        f"get_fenton_lms failed to find for `{ metric }`, `{ sex }`, "
-        f"`{ gestational_age_in_days }`"
-    )
+    @classmethod
+    def get_lms(
+        cls, metric: str, sex: str, gestational_age_in_days: int
+    ) -> (float, float, float):
+        """returns the LMS parameters from Fenton tables.
 
+        Fenton TR and Kim JH, BMC Pediatrics 2013, 13:59
+        http://www.ncbi.nlm.nih.gov/pubmed/23601190
 
-def calc_zscore(
-    metric: str, sex: str, gestational_age_in_days: int, measure: float
-) -> float:
-    """calculates a z-score using Fenton tables and the LMS methodology.
+cls.lms_rows = rows
 
-    Fenton TR and Kim JH, BMC Pediatrics 2013, 13:59
-    http://www.ncbi.nlm.nih.gov/pubmed/23601190
+        Arguments:
+            metric {str} -- Metric to calculate z-score for. Must be `weight`, `length`,
+                or `hc` (head circumference). Units must be in grams for weight, or cm
+                for length or hc.
+            sex {str} -- Patient sex. Must be `f` or `m`.
+            gestational_age_in_days {int} -- Patient gestational age. Ie, to calculate
+                z-scores for a baby born at 24 weeks 0d on dol 5:
+                gestational_age_in_days = 24 * 7 + 5 = 173
 
-    Arguments:
-        metric {str} -- Metric to calculate z-score for. Must be `weight`, `length`,
-            or `hc` (head circumference). Units must be in grams for weight, or cm
-            for length or hc.
-        sex {str} -- Patient sex. Must be `f` or `m`.
-        gestational_age_in_days {int} -- Patient gestational age. Ie, to calculate
-            z-scores for a baby born at 24 weeks 0d on dol 5:
-            gestational_age_in_days = 24 * 7 + 5 = 173
-        measure {float} -- the patient's value of the meausurement for which the
-            z-score shall be calculated.
+        Returns:
+            float -- l parameter
+            float -- m parameter (mean)
+            float -- s parameter
+        """
+        if cls.lms_rows is None:
+            cls.lms_rows = cls._get_lms_rows()
 
-    Returns:
-        float -- z-score for the given inputs
-    """
+        for row in cls.lms_rows:
+            if (
+                row["metric"] == metric
+                and row["sex"] == sex
+                and row["gestational_age_in_days"] == gestational_age_in_days
+            ):
+                return row["l"], row["m"], row["s"]
 
-    # get_fenton_lms will raise a value error if any parameters are bad
-    l, m, s = get_lms(metric.lower(), sex.lower(), int(gestational_age_in_days))
+        raise ValueError(
+            f"get_fenton_lms failed to find for `{ metric }`, `{ sex }`, "
+            f"`{ gestational_age_in_days }`"
+        )
 
-    return ((measure / m) ** l - 1) / (l * s)
+    @classmethod
+    def calc_zscore(
+        cls, metric: str, sex: str, gestational_age_in_days: int, measure: float
+    ) -> float:
+        """calculates a z-score using Fenton tables and the LMS methodology.
+
+        Fenton TR and Kim JH, BMC Pediatrics 2013, 13:59
+        http://www.ncbi.nlm.nih.gov/pubmed/23601190
+
+        Arguments:
+            metric {str} -- Metric to calculate z-score for. Must be `weight`, `length`,
+                or `hc` (head circumference). Units must be in grams for weight, or cm
+                for length or hc.
+            sex {str} -- Patient sex. Must be `f` or `m`.
+            gestational_age_in_days {int} -- Patient gestational age. Ie, to calculate
+                z-scores for a baby born at 24 weeks 0d on dol 5:
+                gestational_age_in_days = 24 * 7 + 5 = 173
+            measure {float} -- the patient's value of the meausurement for which the
+                z-score shall be calculated.
+
+        Returns:
+            float -- z-score for the given inputs
+        """
+
+        # get_fenton_lms will raise a value error if any parameters are bad
+        l, m, s = cls.get_lms(metric.lower(), sex.lower(), int(gestational_age_in_days))
+
+        return ((measure / m) ** l - 1) / (l * s)
--- tests/test_fenton.py
+++ tests/test_fenton.py
@@ -1,25 +1,25 @@
 import pytest
 
-from pretermgrowth.fenton import calc_zscore, get_lms
+from pretermgrowth.fenton import Fenton
 
 
 def test_calc_zscore_good_params():
-    calc_zscore("weight", "f", 171, 2400)  # no ValueError raised
-    calc_zscore("wEIGHt", "F", 171.0, 2400)  # no ValueError raised
+    Fenton.calc_zscore("weight", "f", 171, 2400)  # no ValueError raised
+    Fenton.calc_zscore("wEIGHt", "F", 171.0, 2400)  # no ValueError raised
 
 
 def test_calc_zscore_vs_peditools():
     # comparing results against https://peditools.org/fenton2013/index.php
-    assert round(calc_zscore("length", "m", 280, 54), 2) == 1.27
-    assert round(calc_zscore("weight", "m", 280, 2400), 2) == -2.71
-    assert round(calc_zscore("hc", "m", 280, 33), 2) == -1.44
-    assert round(calc_zscore("length", "f", 277, 54), 2) == 1.71
-    assert round(calc_zscore("weight", "f", 277, 2400), 2) == -2.17
-    assert round(calc_zscore("hc", "f", 277, 33), 2) == -1.06
+    assert round(Fenton.calc_zscore("length", "m", 280, 54), 2) == 1.27
+    assert round(Fenton.calc_zscore("weight", "m", 280, 2400), 2) == -2.71
+    assert round(Fenton.calc_zscore("hc", "m", 280, 33), 2) == -1.44
+    assert round(Fenton.calc_zscore("length", "f", 277, 54), 2) == 1.71
+    assert round(Fenton.calc_zscore("weight", "f", 277, 2400), 2) == -2.17
+    assert round(Fenton.calc_zscore("hc", "f", 277, 33), 2) == -1.06
 
 
 def test_get_lms_valid():
-    l, m, s = get_lms("weight", "f", 171)
+    l, m, s = Fenton.get_lms("weight", "f", 171)
     assert l == 0.664117185
     assert m == 642.3413675
     assert s == 0.169472881
@@ -27,10 +27,10 @@ def test_get_lms_valid():
 
 def test_get_lms_invalid():
     with pytest.raises(ValueError, match=".*fake_metric.*"):
-        get_lms("fake_metric", "m", 177)
+        Fenton.get_lms("fake_metric", "m", 177)
     with pytest.raises(ValueError, match=".*fake_sex.*"):
-        get_lms("weight", "fake_sex", 177)
+        Fenton.get_lms("weight", "fake_sex", 177)
     with pytest.raises(ValueError, match=".*164.*"):
-        get_lms("length", "m", 164)
+        Fenton.get_lms("length", "m", 164)
     with pytest.raises(ValueError, match=".351.*"):
-        get_lms("hc", "f", 351)
+        Fenton.get_lms("hc", "f", 351)
